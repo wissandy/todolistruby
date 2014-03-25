725eecf06bcc60ab08adb8094573224a4f182189
wissandy <wisnu.arisandy@gmail.com>
2014-03-25 09:19:07 -0400

    initial commit of rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Coba::Application.config.secret_key_base = '5af050d71087227ecf3224f50b0b4d02a96c2a338e19f42b9cbda846a115d18875b393d65fb002effa9988e766d1064b69e801aec9384d4d411e6bf3a155229f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Coba::Application.config.session_store :cookie_store, key: '_coba_session'
